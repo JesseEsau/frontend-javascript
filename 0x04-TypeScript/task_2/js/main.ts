412b1839f48a0d195cdd7358ca8919fea36e724d
Esau Jesse <jesseesau13@gmail.com>
2025-05-30 13:09:57 +0100

    Create string literal and function

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -58,3 +58,15 @@ function executeWork(employee: Director | Teacher): string {
         return employee.workTeacherTasks();
     }
 }
+
+// string literal type
+type Subjects = 'Math' | 'History';
+
+function teachClass(todayClass: Subjects): string {
+    if (todayClass === 'Math') {
+        return 'Teaching Math';
+    } else if (todayClass === 'History') {
+        return 'Teaching History'
+    }
+    throw new Error('Invalid subject');
+}
